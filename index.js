a4d3d15480bde904829ae3a1ff61b7454da830fd
codemekh@gmail.com <codemekh@gmail.com>
2020-04-03 23:17:37 +0700

    init

---
+++ index.js
@@ -0,0 +1,20 @@
+var fs = require('fs');
+var express = require('express');
+var app = express();
+
+app.listen(3000, function () {
+  console.log('Listening on port 3000!');
+});
+
+app.get('/getFiveImages', async (req, res) => {
+    try {
+        var array = fs.readFileSync('./images/links.txt').toString().split("\n");   
+        for(var i = 0; i < array.length-1; i++){
+            array[i] = array[i].substr(0, array[i].length-1);
+        }
+        res.send(array);
+    } catch (e) {
+        res.status(404);
+        res.send('File not found!');
+    }
+});
