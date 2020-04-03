47e722f2b3bc97294b167b8256ddf653dedabf52
codemekh@gmail.com <codemekh@gmail.com>
2020-04-03 23:43:40 +0700

    Исправил проблему с портом

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ var fs = require('fs');
 var express = require('express');
 var app = express();
 
-app.listen(3000, function () {
-  console.log('Listening on port 3000!');
+app.listen((process.env.PORT || 3000), function () {
+  console.log('Listening on port ' + (process.env.PORT || 3000));
 });
 
 app.get('/', async (req, res) => {
