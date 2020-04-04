9a5c61eccbe5faf195cffcbc5a53ebbeb073198b
codemekh@gmail.com <codemekh@gmail.com>
2020-04-04 19:13:14 +0700

    Исправил ссылку на бд

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 exports.config = {
-    MONGO_URI: process.env.MONGO_URI || 'mongodb://localhost:27017/memoryhack',
+    MONGO_URI: process.env.MONGODB_URI || 'mongodb://localhost:27017/memoryhack',
     serverPort: process.env.PORT || 1335,
     photoDir: './photos/'
   };
